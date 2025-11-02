fe4e3eaa8b660a487369a26bfa55b1ebf0bdbb3f
Code_with_Manav <codewithmanav70@gmail.com>
2025-11-02 14:39:46 +0530

    final touches

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-// Use the SWC-based React plugin which is already listed in devDependencies
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Metal-Trading-Website/", // ✅ add leading and trailing slashes
 });
